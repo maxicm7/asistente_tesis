d6a0dd134b1ff0058f1e6b955768f8af69321f0e
maxicm7 <118692992+maxicm7@users.noreply.github.com>
2025-09-05 16:18:31 -0300

    dotenv

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # ==============================================================================
 import streamlit as st
 import os
-import dotenv
 from dotenv import load_dotenv
 import google.generativeai as genai
 
