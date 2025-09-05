49bb331331342a004e8ab365f27ccd8e6f7d1ec0
maxicm7 <118692992+maxicm7@users.noreply.github.com>
2025-09-05 16:39:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # ==============================================================================
 import streamlit as st
 import os
-from dotenv import load_dotenv
 import google.generativeai as genai
 
 # --- Importaciones de LangChain ---
